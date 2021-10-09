a860fe3cd1b3b7f1ae0b3378894382e812773a48
MehdiEidi <mehdiadq@gmail.com>
2021-10-09 15:05:25 +0330

    close added

---
+++ IO/close/close.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(void) {
+    int fd = open("/home/mehdi/text.txt", O_WRONLY | O_TRUNC | O_CREAT);
+    if (fd == -1) {
+        // handle open error
+    }
+
+    if (close(fd) == -1) {
+        // handle close error
+    }
+}
